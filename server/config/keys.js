da4d2d0b190faec088ddf921df78e588fb9ec7e1
tiansang <12652350@student.uts.edu.au>
2018-08-13 23:56:36 +1000

    add token

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,7 @@
 
 module.exports = {
-    mongoURI: 'mongodb://Tian:tian1234@ds219832.mlab.com:19832/webdb'
+    mongoURI: 'mongodb://Tian:tian1234@ds219832.mlab.com:19832/webdb',
     //database link
+
+    secretOrkey: 'secret'
 };
