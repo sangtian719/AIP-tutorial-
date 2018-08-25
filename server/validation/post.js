cc48316cde73302332c76ac27d7f72ac238a2a8e
tiansang <12652350@student.uts.edu.au>
2018-08-26 01:45:46 +1000

    add Post validation

---
+++ server/validation/post.js
@@ -0,0 +1,22 @@
+const validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validationPost(data) {
+    let errors = {};
+
+    data.text = !isEmpty(data.text) ? data.text : '';
+
+    if(!validator.isLength(data.text, {min: 15, max: 300})){
+        errors.text = 'Text must be at least 15 and 300 characters';
+    }
+
+    if(validator.isEmpty(data.text)){
+        errors.text = 'Text field is required';
+    }
+
+
+    return{
+        errors,
+        isValid: isEmpty(errors)
+    };
+};
