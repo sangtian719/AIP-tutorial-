9c8a2e60f6d6e5fc3f244ed6eb0d6b40bedc4df7
tiansang <12652350@student.uts.edu.au>
2018-08-25 21:12:12 +1000

    add experience route and validation

--- server/route/api/profile.js
+++ server/route/api/profile.js
@@ -183,6 +183,28 @@ router.post ('/', passport.authenticate('jwt',
         })
     });
 
+//  @route  POST api/profile/experience
+//  @desc   Add experience by profile
+//  @access Private
+
+router.post('/experience', passport.authenticate('jwt', {session: false}), (req, res) =>{
+    Profile.findOne({user: req.user.id})
+        .then(profile => {
+            const newExp = {
+                title: req.body.title,
+                company: req.body.company,
+                location: req.body.location,
+                from: req.body.from,
+                to: req.body.to,
+                current: req.body.current,
+                description: req.body.description
+            };
+            // Add to exp array
+            profile.experience.unshift(newExp);
+
+            profile.save().then(profile => res.json(profile));
+        })
+});
 
 
 
