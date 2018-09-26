1f98d885575d0ada274d1ba8c6e1dc77cd4414c4
tiansang <12652350@student.uts.edu.au>
2018-09-26 19:52:28 +1000

    Redirect function with Authentication function

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,7 +1,19 @@
 import React, {Component} from 'react';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 class Landing extends Component {
+
+
+    componentDidMount()
+    {
+        if(this.props.auth.isAuthenticated)
+        {
+            this.props.history.push('/dashboard');
+        }
+    }
+
     render() {
         return (
             <div className = "landing">
@@ -33,4 +45,13 @@ class Landing extends Component {
     }
 }
 
-export default Landing;
+Landing.propTypes = {
+    auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+
+export default connect(mapStateToProps)(Landing);
